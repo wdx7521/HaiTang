f1195d071efed41c3e8257689a226145f8d873d2
wangdongxing <97521311@qq.com>
2019-11-29 14:52:45 +0800

    登录界面建立

---
+++ app/src/main/java/com/dx/ht/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.dx.ht;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * @create: 2019/11/29.
+ * @author: Wang DongXing
+ * @description: ${description}
+ */
+
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
